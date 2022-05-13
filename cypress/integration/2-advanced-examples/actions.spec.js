a45469989c8ce21f7a113396f5c3804413f691a8
odumy <odumy@softserveinc.com>
2022-05-13 13:39:50 +0300

    Revert "added changes"
    This reverts commit 785c232ce0b5624ac6e92bcdf0a3dd329b9769e1.

--- cypress/integration/2-advanced-examples/actions.spec.js
+++ cypress/integration/2-advanced-examples/actions.spec.js
@@ -6,8 +6,6 @@ context('Actions', () => {
   })
 
   // https://on.cypress.io/interacting-with-elements
-  // https://on.cypress.io/interacting-with-elements
-  // https://on.cypress.io/interacting-with-elements
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
